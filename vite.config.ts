9a8bb144cf1f0564b2b522cfbd1926c0ab19ce74
amexxes <143995809+amexxes@users.noreply.github.com>
2026-05-02 08:53:02 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// Proxy in dev so your API calls go to localhost:3000 (your Express server)
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     proxy: {
-      "/api": "http://localhost:3000"
-    }
-  }
+      "/api": "http://localhost:3000",
+    },
+  },
 });
