205c7738620ca75d71010155ee6a4f6611c2fb81
amexxes <143995809+amexxes@users.noreply.github.com>
2026-05-02 09:55:00 +0200

    Create card.tsx

---
+++ src/components/ui/card.tsx
@@ -0,0 +1,68 @@
+import * as React from "react";
+import { cn } from "@/lib/utils";
+
+export const Card = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => {
+  return (
+    <div
+      ref={ref}
+      className={cn(
+        "rounded-2xl border border-slate-200 bg-white shadow-[0_10px_30px_rgba(15,23,42,0.04)]",
+        className
+      )}
+      {...props}
+    />
+  );
+});
+
+Card.displayName = "Card";
+
+export const CardHeader = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("px-6 pt-6 pb-3", className)} {...props} />;
+});
+
+CardHeader.displayName = "CardHeader";
+
+export const CardTitle = React.forwardRef<
+  HTMLHeadingElement,
+  React.HTMLAttributes<HTMLHeadingElement>
+>(({ className, ...props }, ref) => {
+  return (
+    <h3
+      ref={ref}
+      className={cn("text-lg font-semibold tracking-tight text-slate-900", className)}
+      {...props}
+    />
+  );
+});
+
+CardTitle.displayName = "CardTitle";
+
+export const CardDescription = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLParagraphElement>
+>(({ className, ...props }, ref) => {
+  return (
+    <p
+      ref={ref}
+      className={cn("text-sm text-slate-500", className)}
+      {...props}
+    />
+  );
+});
+
+CardDescription.displayName = "CardDescription";
+
+export const CardContent = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => {
+  return <div ref={ref} className={cn("px-6 pb-6", className)} {...props} />;
+});
+
+CardContent.displayName = "CardContent";
