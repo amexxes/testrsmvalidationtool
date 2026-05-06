eb435712b7dfe385332c328a8f028649d309dec8
amexxes <143995809+amexxes@users.noreply.github.com>
2026-05-06 20:34:55 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import * as Sentry from "@sentry/react";
+import "@dinakars777/react-glass-ui/style.css";
 
 import App from "./App";
 import "./styles.css";
